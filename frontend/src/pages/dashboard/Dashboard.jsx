3510736d72b4392f0d719783dd1958efae35bfe3
py428 <sibgatullah126@gmail.com>
2024-07-11 23:32:26 +0600

    creating dashboard

---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./dashbord.css";
+import CourseCard from "../../components/coursecard/CourseCard";
+const Dashbord = () => {
+  return (
+    <div className="student-dashboard">
+      <h2>All Enrolled Courses</h2>
+      <div className="dashboard-content">
+      </div>
+    </div>
+  );
+};
+
+export default Dashbord;
